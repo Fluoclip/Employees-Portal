92bea3b79c0c8d48337cfcd0f052c189d9abe1db
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-03-29 18:28:21 +0200

    Setup base models

--- src/main/java/com/example/employmanager/configuration/models/Employee.java
+++ src/main/java/com/example/employmanager/configuration/models/Employee.java
@@ -1,9 +1,14 @@
 package com.example.employmanager.configuration.models;
 
 public class Employee {
-    private Integer Id;
-    private String Name;
+
+    private Integer ID;
+    private String firstName;
+    private String lastName;
     private String Street;
     private String City;
-    private String ContractBelongsTo;
+    private Integer telefonNum;
+    private Integer bewacherId;
+    private String lvlOfEducation;
+
 }
--- src/main/java/com/example/employmanager/configuration/models/JobSItes.java
+++ src/main/java/com/example/employmanager/configuration/models/JobSItes.java
@@ -1,4 +1,9 @@
 package com.example.employmanager.configuration.models;
 
 public class JobSItes {
+    private Integer Id;
+    private String Name;
+    private String Street;
+    private String City;
+    private String ContractBelongsTo;
 }
