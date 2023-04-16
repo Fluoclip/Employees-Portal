a9b147e1818c0d6374ab0995b51a1ad374e70a3e
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-04-16 22:27:30 +0200

    setting up POST method

--- src/main/java/fluoworks/online/employee/Employee.java
+++ src/main/java/fluoworks/online/employee/Employee.java
@@ -1,18 +1,45 @@
 package fluoworks.online.employee;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-@NoArgsConstructor
+import java.util.Objects;
+
 
 @Entity
+@Table(name = "employee")
+
 public class Employee {
+
+    @Id
+    @SequenceGenerator(
+            name = "employee_id_sequence",
+            sequenceName = "employee_id_sequence"
+
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_id_sequence"
+    )
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String Street;
+
+    private String City;
+
+    private Integer telefon;
+
+    private String email;
+
+    private Integer bewacherId;
+
+    private String lvlOfEducation;
+
+
     public Employee(Integer id, String firstName, String lastName,
                     String street, String city, Integer telefon,
                     String email, Integer bewacherId, String lvlOfEducation) {
@@ -26,9 +53,10 @@ public class Employee {
         this.bewacherId = bewacherId;
         this.lvlOfEducation = lvlOfEducation;
     }
-    public Employee( String firstName, String lastName,
-                     String street, String city, Integer telefon,
-                     String email, Integer bewacherId, String lvlOfEducation) {
+
+    public Employee(String firstName, String lastName,
+                    String street, String city, Integer telefon,
+                    String email, Integer bewacherId, String lvlOfEducation) {
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -40,32 +68,109 @@ public class Employee {
         this.lvlOfEducation = lvlOfEducation;
     }
 
-    @Id
-    @SequenceGenerator(
-            name = "employee_id_sequence",
-            sequenceName = "employee_id_sequence"
-
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "employee_id_sequence"
-    )
+    public Employee() {
+    }
 
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    private String Street;
-    private String City;
-    private Integer telefon;
-    private String email;
-    private Integer bewacherId;
-    private String lvlOfEducation;
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String street) {
+        Street = street;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public Integer getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(Integer telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getBewacherId() {
+        return bewacherId;
+    }
+
+    public void setBewacherId(Integer bewacherId) {
+        this.bewacherId = bewacherId;
+    }
+
+    public String getLvlOfEducation() {
+        return lvlOfEducation;
+    }
+
+    public void setLvlOfEducation(String lvlOfEducation) {
+        this.lvlOfEducation = lvlOfEducation;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", Street='" + Street + '\'' +
+                ", City='" + City + '\'' +
+                ", telefon=" + telefon +
+                ", email='" + email + '\'' +
+                ", bewacherId=" + bewacherId +
+                ", lvlOfEducation='" + lvlOfEducation + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(Street, employee.Street) && Objects.equals(City, employee.City) && Objects.equals(telefon, employee.telefon) && Objects.equals(email, employee.email) && Objects.equals(bewacherId, employee.bewacherId) && Objects.equals(lvlOfEducation, employee.lvlOfEducation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, Street, City, telefon, email, bewacherId, lvlOfEducation);
+    }
 
 
 }
