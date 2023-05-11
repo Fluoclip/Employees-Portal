2f217ef6b844eefd9aa339e234d2fbb7163b3544
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-05-12 00:55:25 +0200

    random num of emp

---
+++ src/main/java/fluoworks/online/employee/EmployeeRandomGenerator.java
@@ -0,0 +1,28 @@
+package fluoworks.online.employee;
+
+public class EmployeeRandomGenerator {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeRandomGenerator(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public void addSpecificNumOfEmployees(int numOfEmp){
+
+        for( int i =0; i < numOfEmp; i++){
+
+            Employee employee = new Employee();
+
+            employee.setFirstName("ime" + i);
+            employee.setLastName("prezime" + i);
+            employee.setTelefon(3213214);
+            employee.setEmail("ime"+ "prezime" + i + "@gmail.com");
+            employee.setBewacherId(21313);
+            employee.setLvlOfEducation("Sachkunde");
+
+            employeeRepository.save(employee);
+
+        }
+    }
+}
